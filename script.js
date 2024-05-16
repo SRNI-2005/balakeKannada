98225bbd7a1042a5c3f45deb8dcf81b7f639d56c
Sumedh <sumedhudupa15@gmail.com>
2024-05-16 11:06:05 +0530

    committed

--- script.js
+++ script.js
@@ -1,40 +1,99 @@
-//script
-document.getElementsByClassName("question-content")[0].innerHTML = "______. yaava uuru?"
-let a = ["niinu","nimma","niMa","neenu"]
-let ans = ["niinu"]
-let numberOfCorrect = 0
-let numberOfWrong = 0
-let s = document.getElementsByClassName("option");
-for(let i=0;i<s.length;i++){
-    s[i].innerHTML = a[i];
-}
-function color(d) {
-    d.setAttribute("color","green");
-}
-let clicked = document.getElementsByClassName("option")
-for(let i=0;i<s.length;i++){
-    if(clicked[i].innerHTML === ans[0]){
-        clicked[i].addEventListener("click",()=>{
-            clicked[0].style.backgroundColor = "green"
-            numberOfCorrect++
-            document.getElementsByClassName("correct-score")[0].innerHTML="Correct answers:" + numberOfCorrect
-            for(let j=0;j<s.length;j++){
-                clicked[j].setAttribute("disabled","true")
-            }
-        })
-        
-    }
-    else{
-        clicked[i].addEventListener("click",()=>{
-            clicked[i].style.backgroundColor = "red"
-            numberOfWrong++;
-            document.getElementsByClassName("incorrect-score")[0].innerHTML="Incorrect answers:" + numberOfWrong
-            for(let j=0;j<s.length;j++){
-                clicked[j].setAttribute("disabled","true")
-            }
-        })
-        
-    }
-} 
+//
+const domCorrectScore = document.querySelector(".correct-score");
+const domIncorrectScore = document.querySelector(".incorrect-score");
+const domQuestionNumber = document.querySelector(".question-number");
 
+const questionContent = document.querySelector(".question-content");
+
+let options = document.querySelectorAll(".option");
+
+let option1 = options[0];
+let option2 = options[1];
+let option3 = options[2];
+let option4 = options[3];
+
+let questions = ["____ yaava uuru?","namma kaaleeju tumba …………………………………..","ivattu ……………………. Pariikshe(test) ide.","bengaLuuru ………………… raajadhaani(capital city).","…………………… namma shikshakaru.","run: ooDu :: read …………….","kannaDa form of orange colour……………..","kannaDa form of 4th is ……………..","opposite of eDagaDe X ………….","opposite of hosa X …………."];
+let correctAns = ["niinu", "cannaagide.","namage","namma","avaru","oodu","ippattu","naalkanee","balagaDe"];
+let incorrectAns1 = ["nimma", "cannaagideyaa?","naavu","naanu","idu","OOdu","eppattu","naalkara","bala",];
+let incorrectAns2 = ["niMa", "cannaagiddiini.","niinu","nanna","ivara","odu","eraDu sonne","naalku","balakke"];
+let incorrectAns3 = ["neenu", "cannaagilla","naanu","nannadu","idara","oDu","yeppattu","naalakku","balakaDe"];
+
+let correctScore = 0;
+let incorrectScore = 0;
+let questionIndex = 0;
+
+function shuffleOptions(array){ //function to shuffle array elements
+    for (let i=array.length-1; i>0; i--){
+        const j = Math.floor(Math.random()*(i+1)); //random index to swap
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function quiz(answerSelected, button){
+    if (questionIndex<questions.length){
+        if (answerSelected === correctAns[questionIndex]){
+            updateScore(true);
+        } else {
+            updateScore(false);
+        }
+        ++questionIndex;
+        if (questionIndex == questions.length){
+            alert("Quiz completed, your score: " + (correctScore/(correctScore+incorrectScore)*100));
+            return;
+        } else {
+            updateQuestion();
+            updateOptions();
+            
+        }
+    }
+}
+
+function updateQuestion(){
+    let currentQuestion = questions[questionIndex];
+    questionContent.textContent = currentQuestion;
+    domQuestionNumber.textContent = "Question: "+ (questionIndex+1);
+}
+function updateOptions(){
+    let choices = [correctAns[questionIndex], incorrectAns1[questionIndex], incorrectAns2[questionIndex], incorrectAns3[questionIndex]];
+    shuffleOptions(choices);
+    for (let i=0; i<choices.length; i++){
+        options[i].textContent = choices[i];
+    }
+}
+function updateScore(isCorrect){
+    if (isCorrect){
+        domCorrectScore.textContent = "Correct answers: " + ++correctScore;
+    } else {
+        domIncorrectScore.textContent = "Incorrect answers: "+ ++incorrectScore;
+    }
+}
+
+updateQuestion();
+updateOptions();
+
+option1.addEventListener("click", () => quiz(option1.textContent, option1));
+option2.addEventListener("click", () => quiz(option2.textContent, option2));
+option3.addEventListener("click", () => quiz(option3.textContent, option3));
+option4.addEventListener("click", () => quiz(option4.textContent, option4));
+
+document.onkeydown = function(e) { //Code to prevent inspection using keys
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+document.addEventListener('contextmenu', event => event.preventDefault()); //code to prevent inspection using right click
 
